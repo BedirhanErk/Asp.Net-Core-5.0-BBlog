9bcfda915fee565c1bbd44c8160b352d4444a404
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-11-26 00:02:34 +0300

    Add project files.

---
+++ BBlog.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BBlog.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        public int AboutId { get; set; }
+        public string Detail1 { get; set; }
+        public string Detail2 { get; set; }
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string MapLocation { get; set; }
+        public bool Status { get; set; }
+    }
+}
