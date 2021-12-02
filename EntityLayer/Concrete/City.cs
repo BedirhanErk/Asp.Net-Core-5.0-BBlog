20ef9bf9565209074161f8c3de2bf91c91ebd2e9
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-12-03 00:01:42 +0300

    City Güncellemeleri

---
+++ EntityLayer/Concrete/City.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+        public string Name { get; set; }
+    }
+}
