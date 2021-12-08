6c579fd116fffe114cf439357a48bcc612782a8e
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-12-08 22:34:52 +0300

    Error Güncellemeleri

---
+++ BBlog.UI/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult ErrorPage1(int code)
+        {
+            return View();
+        }
+    }
+}
