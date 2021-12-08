7f573313006a74383bd9f5d369014961aab23f8e
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-12-08 23:00:47 +0300

    About Güncellemeleri

---
+++ BBlog.UI/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = abm.GetAll();
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
