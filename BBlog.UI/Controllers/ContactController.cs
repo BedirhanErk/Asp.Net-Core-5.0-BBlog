8386b18b0f04f16de1a1c28a37bdf80a22cefe16
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-12-08 23:26:06 +0300

    Comment Güncellemeleri.

---
+++ BBlog.UI/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BBlog.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.CreDate = DateTime.Now;
+            contact.Status = true;
+            cm.Add(contact);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            _contactDal = contactDal;
+        }
+
+        public void Add(Contact contact)
+        {
+            _contactDal.Add(contact);
+        }
+    }
+}
