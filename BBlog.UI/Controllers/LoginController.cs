206d3fbb1956e335544117e281ed6d862d3e6a66
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-12-11 20:16:39 +0300

    Login Güncellemeleri

--- BBlog.UI/Controllers/LoginController.cs
+++ BBlog.UI/Controllers/LoginController.cs
@@ -1,34 +1,55 @@
 ﻿using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BBlog.UI.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        [HttpGet]    
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index(Writer writer)
+        public async Task<IActionResult> Index(Writer writer)
         {
             Context c = new Context();
-            var dataValue = c.Writers.FirstOrDefault(x=>x.Mail == writer.Mail && x.Password == writer.Password);
+            var dataValue = c.Writers.FirstOrDefault(x => x.Mail == writer.Mail && x.Password == writer.Password);
             if (dataValue != null)
             {
-                HttpContext.Session.SetString("username", writer.Mail);
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, writer.Mail)
+                };
+                var userIdentity = new ClaimsIdentity(claims, "a");
+                ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                await HttpContext.SignInAsync(principal);
                 return RedirectToAction("Index", "Blog");
             }
             else
             {
                 return View();
-            }           
+            }
         }
     }
 }
+//Context c = new Context();
+//var dataValue = c.Writers.FirstOrDefault(x => x.Mail == writer.Mail && x.Password == writer.Password);
+//if (dataValue != null)
+//{
+//    HttpContext.Session.SetString("username", writer.Mail);
+//    return RedirectToAction("Index", "Blog");
+//}
+//else
+//{
+//    return View();
+//}
--- BBlog.UI/Startup.cs
+++ BBlog.UI/Startup.cs
@@ -1,15 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BBlog.UI
 {
@@ -37,6 +33,15 @@ namespace BBlog.UI
                               .Build();
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddMvc();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,10 +58,12 @@ namespace BBlog.UI
                 app.UseHsts();
             }
 
-            app.UseStatusCodePagesWithReExecute("/Error/ErrorPage1","?code={0}");
+            app.UseStatusCodePagesWithReExecute("/Error/ErrorPage1", "?code={0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseSession();
 
