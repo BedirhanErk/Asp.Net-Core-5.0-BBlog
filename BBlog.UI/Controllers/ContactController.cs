7b853e6cafca596f4c122847e2ece28b1808dc5f
BedirhanErk <erkilicbedirhanlol@gmail.com>
2021-12-15 21:24:40 +0300

    Logo ve Icon Güncellemeleri

--- BBlog.UI/Controllers/ContactController.cs
+++ BBlog.UI/Controllers/ContactController.cs
@@ -1,11 +1,13 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace BBlog.UI.Controllers
 {
+    [AllowAnonymous]
     public class ContactController : Controller
     {
         ContactManager cm = new ContactManager(new EfContactRepository());
--- BBlog.UI/Controllers/RegisterController.cs
+++ BBlog.UI/Controllers/RegisterController.cs
@@ -4,11 +4,12 @@ using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BBlog.UI.Controllers
 {
+    [AllowAnonymous]
     public class RegisterController : Controller
     {
         WriterManger wm = new WriterManger(new EfWriterRepository());
