0022bf6c67bd68b3883f616262a0a56b2111f4e0
BedirhanErk <erkilicbedirhan@gmail.com>
2022-01-14 23:58:52 +0300

    Dashboard Güncellemeleri v1

---
+++ BBlog.UI/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.Controllers
+{
+    [AllowAnonymous]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
