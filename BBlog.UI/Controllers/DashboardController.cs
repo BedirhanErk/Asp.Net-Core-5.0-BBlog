adf7b0763664452eeb424d1301baeee6eb27b9e0
BedirhanErk <erkilicbedirhan@gmail.com>
2022-01-16 16:05:30 +0300

    Writer Dashboard Güncellemeleri

--- BBlog.UI/Controllers/DashboardController.cs
+++ BBlog.UI/Controllers/DashboardController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BBlog.UI.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
+
             return View();
         }
     }
---
+++ BBlog.UI/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.ViewComponents.Writer
+{
+    public class WriterAboutOnDashboard : ViewComponent
+    {
+        WriterManger wm = new WriterManger(new EfWriterRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = wm.GetWriterById(4);
+            return View(values);
+        }
+    }
+}
