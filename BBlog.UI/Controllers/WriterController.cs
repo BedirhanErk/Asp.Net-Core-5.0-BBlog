8ee92b05b8d1c5c9b7f2d99c7f627e9664a3b947
BedirhanErk <erkilicbedirhan@gmail.com>
2022-01-22 00:31:54 +0300

    Yazar Profil Güncellemeleri

--- BBlog.UI/Controllers/WriterController.cs
+++ BBlog.UI/Controllers/WriterController.cs
@@ -1,13 +1,44 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BBlog.UI.Controllers
 {
     public class WriterController : Controller
     {
+        WriterManger wm = new WriterManger(new EfWriterRepository());
+        [HttpGet]
         public IActionResult Index()
         {
-            
+            var writerValues = wm.GetById(4);
+            return View(writerValues);
+        }
+        [HttpPost]
+        public IActionResult Index(Writer writer, string ConfirmPassword)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult result = wv.Validate(writer);
+            if (result.IsValid && (writer.Password == ConfirmPassword))
+            {
+                var values = wm.GetById(writer.WriterId);
+                writer.Status = values.Status;
+                wm.Update(writer);
+                return RedirectToAction("Index");
+            }
+            else if (writer.Password != ConfirmPassword)
+            {
+                ModelState.AddModelError("Password", "Passwords do not match, please try again");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
         public PartialViewResult PartialWriterLeftNavbar()
