63bd5b977305969dd47d7388d043cec7f5f301f2
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-09 00:46:14 +0300

    Admin Paneli Güncellemeleri - Tema Dosyaları

---
+++ BBlog.UI/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AdminLeftNavbar()
+        {
+            return PartialView();
+        }
+    }
+}
