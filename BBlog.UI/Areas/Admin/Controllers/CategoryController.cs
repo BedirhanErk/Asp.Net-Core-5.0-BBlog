b2847831d118b6bac4aa57e56418b56cd77b4fd2
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-12 21:26:52 +0300

    Admin Paneli İçin Areas Tanımlanması ve Kategori İşlemleri

---
+++ BBlog.UI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetAll();
+            return View(values);
+        }
+    }
+}
--- BBlog.UI/Startup.cs
+++ BBlog.UI/Startup.cs
@@ -74,6 +74,10 @@ namespace BBlog.UI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Blog}/{action=Index}/{id?}");
             });
