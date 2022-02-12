63f2169bb93cbc5b99a34532f7436a3c6cc7b7fb
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-12 22:42:48 +0300

    Kategori Güncellemeleri

--- BBlog.UI/Areas/Admin/Controllers/CategoryController.cs
+++ BBlog.UI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace BBlog.UI.Areas.Admin.Controllers
 {
@@ -8,10 +12,49 @@ namespace BBlog.UI.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        CategoryValidator cv = new CategoryValidator();
+        public class ParamId
         {
-            var values = cm.GetAll();
+            public int id { get; set; }
+        }
+        public IActionResult Index(int page=1)
+        {
+            var values = cm.GetAll().ToPagedList(page, 10);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            ValidationResult result = cv.Validate(category);
+            if(result.IsValid)
+            {
+                category.Status = true;
+                cm.Add(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteCategory([FromBody] ParamId request)
+        {
+            var value = cm.GetById(request.id);
+            if (value.Status == true)
+                value.Status = false;
+            else
+                value.Status = true;
+            cm.Update(value);
+            return Json(value);
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Category name cannot be empty.");
+            RuleFor(x => x.Name).MaximumLength(50).WithMessage("Category name must be a maximum of 50 characters.");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("Category name must be at least 2 characters.");
+        }
+    }
+}
