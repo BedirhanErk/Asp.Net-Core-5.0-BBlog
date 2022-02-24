63385840ebc79520e8ba8eb16008be1a79cf8ddf
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-24 22:42:30 +0300

    Admin Güncellemeleri

--- BBlog.UI/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ BBlog.UI/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
 
 namespace BBlog.UI.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic4 : ViewComponent
     {
+        AdminManager am = new AdminManager(new EfAdminRepository());
         public IViewComponentResult Invoke()
-        {
-            return View();
+        {           
+            var value = am.GetById(1);
+            return View(value);
         }
     }
 }
---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _adminDal;
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+        public void Add(Admin t)
+        {
+            _adminDal.Add(t);
+        }
+
+        public void Delete(Admin t)
+        {
+            _adminDal.Delete(t);
+        }
+
+        public List<Admin> GetAll()
+        {
+            return _adminDal.GetAll();
+        }
+
+        public Admin GetById(int id)
+        {
+            return _adminDal.GetById(id);
+        }
+
+        public void Update(Admin t)
+        {
+            _adminDal.Update(t);
+        }
+    }
+}
