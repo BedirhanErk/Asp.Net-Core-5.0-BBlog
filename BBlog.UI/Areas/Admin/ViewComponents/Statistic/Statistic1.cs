ff5be8f534912cc45bc6a77d0eae6c4c888d37a4
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-25 21:55:42 +0300

    Weather API Güncellemesi

--- BBlog.UI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ BBlog.UI/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace BBlog.UI.Areas.Admin.ViewComponents.Statistic
 {
@@ -15,6 +16,13 @@ namespace BBlog.UI.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetBlogCount();
             ViewBag.v2 = wm.GetAll().Count();
             ViewBag.v3 = cm.GetCommentCount();
+
+            //NOTE: Şehir kullanıcı üzerinden çekilebilir.
+            string apiId = "b54b3d00e78f2cf8418974a753df5eba";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=Istanbul&mode=xml&lang=tr&units=metric&appid=" + apiId;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
             return View();
         }
     }
