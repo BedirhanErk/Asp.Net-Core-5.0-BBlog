2fac97d6550b1cbf1dd9990ba05b74c7b94b1353
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-25 22:53:15 +0300

    Google Chart Güncellemesi

---
+++ BBlog.UI/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,49 @@
+﻿using BBlog.UI.Areas.Admin.Models;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BBlog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+
+            var categories = cm.GetAll();
+            var blogs = bm.GetBlogListWithCategory();
+
+            foreach (var item in categories)
+            {
+                list.Add(new CategoryClass
+                {
+                    categoryname = item.Name,
+                    categorycount = 0
+                });
+            }
+
+            foreach (var blog in blogs)
+            {
+                foreach (var category in list)
+                {
+                    if (blog.Category.Name == category.categoryname)
+                    {
+                        category.categorycount = category.categorycount + 1;
+                    }
+                }
+            }
+
+            return Json(new { jsonlist = list});
+        }
+    }
+}
