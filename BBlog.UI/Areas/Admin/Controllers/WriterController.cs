8afc94540c777588d8bea0e0097bc96b283914e7
BedirhanErk <erkilicbedirhan@gmail.com>
2022-02-26 15:05:05 +0300

    Yazarların Listelenmesi

---
+++ BBlog.UI/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,41 @@
+﻿using BBlog.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BBlog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetWriterList()
+        {
+            var jsonWriter = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriter);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id = 1,
+                Name = "Ayşe"
+            },
+            new WriterClass
+            {
+                Id = 2,
+                Name = "Ahmet"
+            },
+            new WriterClass
+            {
+                Id = 3,
+                Name = "Buse"
+            }
+        };
+    }
+}
