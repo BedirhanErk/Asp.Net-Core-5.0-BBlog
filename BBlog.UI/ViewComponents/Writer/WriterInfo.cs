ec346ef8c8accaccaf3123797704960122bd93b1
BedirhanErk <erkilicbedirhan@gmail.com>
2022-03-27 14:48:15 +0300

    Yazar Bilgileri Güncellemesi

---
+++ BBlog.UI/ViewComponents/Writer/WriterInfo.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BBlog.UI.ViewComponents.Writer
+{
+    public class WriterInfo : ViewComponent
+    {
+        private readonly UserManager<AppUser> _user;
+        public WriterInfo(UserManager<AppUser> user)
+        {
+            _user = user;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _user.FindByNameAsync(User.Identity.Name);
+            return View(user);
+        }
+    }
+}
---
+++ BBlog.UI/ViewComponents/Writer/WriterInfoLeft.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BBlog.UI.ViewComponents.Writer
+{
+    public class WriterInfoLeft : ViewComponent
+    {
+        private readonly UserManager<AppUser> _user;
+        public WriterInfoLeft(UserManager<AppUser> user)
+        {
+            _user = user;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _user.FindByNameAsync(User.Identity.Name);
+            return View(user);
+        }
+    }
+}
