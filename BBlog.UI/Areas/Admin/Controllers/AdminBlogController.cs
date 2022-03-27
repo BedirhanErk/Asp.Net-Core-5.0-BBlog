385c413f568ee62c758112e0d952e3989fb7be3c
BedirhanErk <erkilicbedirhan@gmail.com>
2022-03-27 18:40:51 +0300

    Blog Güncellemeleri

---
+++ BBlog.UI/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public class ParamBlogId
+        {
+            public int id { get; set; }
+        }
+        public IActionResult Index()
+        {
+            var blogs = bm.GetBlogListWithCategory();
+            return View(blogs);
+        }
+        public IActionResult DeleteBlog([FromBody] ParamBlogId request)
+        {
+            var blog = bm.GetById(request.id);
+            blog.Status = (blog.Status == false) ? true : false;
+            bm.Update(blog);
+            return Json(blog);
+        }
+        public IActionResult UpdateBlog()
+        {
+            return View();
+        }
+        public IActionResult AddBlog()
+        {
+            return View();
+        }
+    }
+}
