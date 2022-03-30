e8441e7bf7f25a48b5f59ba64b1660b6a32db24e
BedirhanErk <erkilicbedirhan@gmail.com>
2022-03-30 22:29:30 +0300

    Role v1

---
+++ BBlog.UI/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,51 @@
+﻿using BBlog.UI.Areas.Admin.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BBlog.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel request)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole();
+                role.Name = request.Name;
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ BBlog.UI/Areas/Admin/Models/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BBlog.UI.Areas.Admin.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+    }
+}
