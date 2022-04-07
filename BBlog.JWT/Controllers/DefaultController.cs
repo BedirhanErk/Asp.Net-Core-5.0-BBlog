846caf09f3c5d01a9827442ab7ec2856b520e68a
BedirhanErk <erkilicbedirhan@gmail.com>
2022-04-07 23:09:31 +0300

    Jwt Güncellemesi

---
+++ BBlog.JWT/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using BBlog.JWT.Dal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBlog.JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("",new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Page1()
+        {
+            return Ok("Sayfa 1 için girişiniz başarılı");
+        }
+    }
+}
